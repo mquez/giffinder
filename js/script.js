a6959cc95f635af679726cfd8e03d0dd690535e7
j13huang <j13huang@gmail.com>
2018-02-20 23:40:20 -0800

    scaffolding

--- js/script.js
+++ js/script.js
@@ -4,7 +4,31 @@
 
 $(document).ready(function(){
   
+  // WRITE A CLICK HANDLER HERE
   
-  
-  
+  function giphyURLWithSearchTerm(searchTerm) {
+      // write a function that will return a url for the giphy API with
+      // the searchTerm provided in the parameters
+  }
+
+  function appendImageToBody(srcURL) {
+      // write a function that will append an <img> to the body with the
+      // URL provided in the parameters
+  }
+
+  function callGiphyAPIWithSearchTerm(searchTerm) {
+      $.ajax({
+        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(response) {
+             // Log the whole response to the console
+
+             // Log the first image of the data to the console
+
+             // Log the "type" property of the first image object to the console
+
+             // Log the "title" property of the first image object to the console
+        },
+      }); 
+  }
 });
