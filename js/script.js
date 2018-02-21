8908a3561076ec6d42ad81b39bff6d2930c4d9fb
j13huang <j13huang@gmail.com>
2018-02-20 23:50:32 -0800

    more comments

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@
 $(document).ready(function(){
   
   // WRITE A CLICK HANDLER HERE
+  /*
+    1. click handler function
+    2. get the typed input from the search input box
+    3. call the functions below!
+  */
   
   function giphyURLWithSearchTerm(searchTerm) {
       // write a function that will return a url for the giphy API with
@@ -17,5 +22,8 @@ $(document).ready(function(){
   }
 
   function callGiphyAPIWithSearchTerm(searchTerm) {
+      // use $.ajax to call the giphy api with the given search term from the parameters.
+      // get the first image url from the ajax response
+      // use appendImageToGallery to put the image onto the screen
   }
 });
